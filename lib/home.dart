62b3fd3618cb7e63172412301c1fd0dc98af82c3
basel <baselhadrous99@gmail.com>
2020-12-01 19:45:46 +0200

    Add DI support + home provider + local storage + routing

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:soshi_app/features/home/home_provider.dart';
 
-import 'di.dart';
 import 'sushi_icons.dart';
 
 class Home extends StatelessWidget {
